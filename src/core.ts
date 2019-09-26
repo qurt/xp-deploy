3e9c18d2af7d34fbf423e056cee78602328e84a5
Andrey Orekhov <orekhov@corp.101xp.com>
2019-09-26 14:37:17 +0300

    Copy last release
    Add method for copy last release folder if this option is set

--- src/core.ts
+++ src/core.ts
@@ -1,6 +1,7 @@
 import { exec } from "child_process";
 import { buildSSHCommand } from "./utils/ssh";
 import chalk from "chalk";
+import * as path from "path";
 
 export const remote = (
   user: string,
@@ -18,11 +19,24 @@ export const local = (cmd: string): Promise<string> => {
       // TODO: Сделать нормальный вывод ошибок
       if (stderr) resolve(stderr);
       resolve(stdout);
-      console.log(`>>> ${stdout}`);
+      if (stdout && stdout.length > 0) {
+        console.log(`>>> ${stdout}`);
+      }
     });
   });
 };
 
 export const tab = (count: number = 4) => {
   return " ".repeat(count);
+};
+export const computeReleaseDirname = (result: string): string => {
+  if (!result) return null;
+
+  // Trim last breakline.
+  const target = result.replace(/\n$/, "");
+  return target.split(path.sep).pop();
+};
+
+export const equalValues = (values: Array<string>): boolean => {
+  return values.every(value => value === values[0]);
 };
